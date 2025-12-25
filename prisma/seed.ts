f113d355df27b78749fcdfe4bf57f51edd797a70
salonijibhe07 <salonijibhe07@gmail.com>
2025-12-25 12:52:28 +0530

    upload

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma";
 import bcrypt from "bcryptjs";
 
 import { PrismaPg } from "@prisma/adapter-pg";
